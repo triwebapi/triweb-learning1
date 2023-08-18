d6b6e1cf36441f473cb737fffab51963b8d1e070
Mohd Danish <alid41549@gmail.com>
2023-08-18 16:55:09 +0530

    added to Learn Datatypes

---
+++ javascript/Day 2/datatypes.js
@@ -0,0 +1,19 @@
+let nullVariable = null;
+
+
+let undefinedVariable;
+
+let numVar = 12;
+
+
+let str = " Hi Danish";
+
+let obj = {};
+
+
+console.log("null",nullVariable,"",typeof(null));
+
+console.log("undefinedvariable", undefinedVariable,"",typeof(undefinedVariable));
+console.log("numVar",numVar,"",typeof(numVar));
+console.log("str", str,"",typeof(str));
+console.log("obj", obj,"",typeof(obj));
