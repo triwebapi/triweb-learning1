25d3734bf9427e932af975015d0a4e46768df8c6
Mohd Danish <alid41549@gmail.com>
2023-08-18 17:47:55 +0530

    added literals

---
+++ javascript/Day 2/datatype conversion.js
@@ -0,0 +1,30 @@
+let n = 1;
+console.log(typeof(n));//number
+
+
+n = "Hi Danish";
+console.log(typeof(n));//string
+
+let num1 = '1';
+let num2 = '4'
+console.log(num1+num2);// string and perform concatinatiuon
+
+
+let num3 = '1'
+let num4 = 4;
+console.log(parseInt(num3)+num4);// number using parseint
+
+
+
+let n5 = '3.5';
+let n6 = 5;
+// console.log(parseInt(n5)+n6);
+console.log(parseFloat(n5)+n6);// 8.5 
+
+let n7 = '8'
+let n8 = 5;
+console.log(+n7+n8); // using + operators we can add string number
+
+n9 = '101';
+console.log(parseInt(n9,2));// 5, it handle 101 as binary
+
---
+++ javascript/Day 2/literals.js
@@ -0,0 +1,34 @@
+console.log("Literals");
+console.log("decimal",117);
+console.log("octal",0o77);
+console.log("hexa decimal",0x00111);
+console.log("binary",0b0011);
+console.log("Binary",0B0011);
+
+console.log('object literals');
+
+
+let obj2 = {name:"Danish",mobile:"8978"};
+console.log(obj2);
+console.log(obj2.name);
+
+
+console.log("object literals with square bracket");
+let obj1 = {'':"Danish",'!':"8978"};
+console.log (obj1['']);
+console.log (obj1['!']);
+
+
+console.log("string literals");
+let s1 = 'string in single quotes';
+let s2 = "string in double quotes";
+
+
+let name1 = 'Danish';
+let s3 = 'hi '+name1+' How r you?';
+let s4 = `hi ${name1}
+how r you?`
+console.log("s1",s1);
+console.log("s2",s2);
+console.log("s3",s3);
+console.log("s4",s4);
