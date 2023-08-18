56b1e1fd2f926ba0a6775144c1d928b1a49ebb26
Mohd Danish <alid41549@gmail.com>
2023-08-18 11:24:07 +0530

    added operational statement

--- javascript/Day 1/operators.js
+++ javascript/Day 1/operators.js
@@ -1,2 +1,22 @@
 var x = 10;
-console.log(x);
+var y = 55;
+var z = 300;
+if (x>y){
+    console.log("x is greater than y");
+    //xz
+    if(x>z){
+        console.log("x is greater than all");
+    }else{
+        console.log("z is greater than all");
+    }
+}
+else{
+    console.log("y is greater than x");
+    //yz
+    if(y>z){
+        console.log("y is greater than all");
+    }else{
+        console.log("z is greater than all");
+
+    }
+}
