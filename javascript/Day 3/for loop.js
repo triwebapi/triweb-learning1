249f28cfccad843c9a44ebbca17d8e786c7e49d1
Mohd Danish <alid41549@gmail.com>
2023-08-19 15:59:43 +0530

    understanding loops

---
+++ javascript/Day 3/for loop.js
@@ -0,0 +1,12 @@
+for (let sn = 1; sn<=10; sn++) {
+    console.log(sn + "india");
+    
+}
+
+console.log("..............");
+
+let sn1;
+for(sn1 = 1; sn1<=10; sn1++){
+    console.log(sn1+"india");
+}
+console.log(sn1);
---
+++ javascript/Day 3/loops.js
@@ -0,0 +1,9 @@
+// let sn = 1 //initialisation
+// if (sn<=10)//condition
+// console.log(sn +"india");//statement
+
+// sn++ // increament
+
+for( let sn = 1; sn<=10; sn++){
+    console.log(sn +"india");
+}
