e08e74be45b6373829ea7e61135720a732ce9f5c
Mohd Danish <alid41549@gmail.com>
2023-08-20 13:25:02 +0530

    added again reursion with diffrenet ways

--- javascript/Day 4/recursionwithoutloop.js
+++ javascript/Day 4/recursionwithoutloop.js
@@ -1,10 +1,38 @@
-function PrintNumSeriesRecursion(num){
+// function PrintNumSeriesRecursion(num){
+//     if(num==0){
+//         return 0;
+//     }
+//     console.log(num);
+//     num--;
+//     PrintNumSeriesRecursion(num);
+     
+// }
+// PrintNumSeriesRecursion(5);// when function call itself again and again this is called recursion.
+
+
+//method-2
+// console.log("................");
+
+// const printSeries = function PrintSeriesRecursion(num){
+//     if(num==0){
+//         return 0;
+//     }
+//     console.log(num);
+//     num--;
+//     PrintSeriesRecursion(num);
+// }
+// printSeries(5);
+
+//method-3
+
+console.log("..........................................");
+
+const PrintSeries1 = function PrintSeriesRecursion1(num){
     if(num==0){
         return 0;
     }
     console.log(num);
     num--;
-    PrintNumSeriesRecursion(num);
-     
+    PrintSeriesRecursion1(num);
 }
-PrintNumSeriesRecursion(5);// when function call itself again and again this is called recursion.
+PrintSeries1(5);
