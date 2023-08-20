a7705ee04551f14291c02dfd86a9e6b6176a28ae
Mohd Danish <alid41549@gmail.com>
2023-08-20 17:15:51 +0530

    added arrow function

---
+++ javascript/Day 5/arrow func.js
@@ -0,0 +1,23 @@
+// function getSquare(num){
+//     return num*num;
+// }
+
+// let sqr = getSquare(4);
+// console.log(sqr);
+
+
+//method-2
+
+
+// const getSquare = function(num){
+//     return num*num;
+// }
+// let sqr = getSquare(4);
+// console.log(sqr);
+
+
+//here i will use arrow function to ease my program.
+
+const getSquare = num =>num*num;
+let sqr = getSquare(4);
+console.log(sqr);
