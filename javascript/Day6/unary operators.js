da5605577e03e474e97b92129441b62772ac5454
Mohd Danish <alid41549@gmail.com>
2023-08-21 10:50:24 +0530

    added unary operator

---
+++ javascript/Day6/unary operators.js
@@ -0,0 +1,16 @@
+let x1 = 4;
+
+let y1 = -x1;
+console.log(y1);
+
+
+let a= '2';
+console.log(+a+3);// using + operator while adding string
+
+// delete
+
+let obj={name:"Danish",mobile:9891};
+console.log(obj);         // use to find object and delete and object.
+
+delete obj.mobile;
+console.log(typeof(obj));
