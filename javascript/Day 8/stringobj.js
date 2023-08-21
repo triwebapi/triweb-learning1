56e3866f3dc5a8063d52d3e3686ea54c840b6e5a
Mohd Danish <alid41549@gmail.com>
2023-08-21 23:20:30 +0530

    added texformating and string objct

---
+++ javascript/Day 8/stringobj.js
@@ -0,0 +1,9 @@
+const strobj = new String("hi i am here");
+console.log(strobj);
+
+
+const strobj1 = String("hi");
+console.log(strobj1);
+
+const str2 = "hi";
+console.log(str2);
---
+++ javascript/Day 8/textformating.js
@@ -0,0 +1,24 @@
+const str = "india";
+console.log(str); // here we can use string in single quotes like 'india'
+// and also use in backtick loke `india`.
+
+
+// if we can to print india in double quote so we can use like .......
+
+const description =  "india is my country";
+console.log(description);
+
+const description1 = '"india" is my country';
+console.log(description1);
+
+
+const description2 = "'india'is my country";
+console.log(description2);
+
+const description3 = "'india' is my"+'"country"';
+console.log(description3);
+
+//lastly we can use .... backslash
+
+const description4 = "'india' is my\"country\"";
+console.log(description4);
