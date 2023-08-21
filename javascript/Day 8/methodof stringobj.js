d8a83996f8a9a0835510f61aac6e5a5ab0f43bc2
Mohd Danish <alid41549@gmail.com>
2023-08-21 23:41:08 +0530

    method of string obj

---
+++ javascript/Day 8/methodof stringobj.js
@@ -0,0 +1,19 @@
+const str3 = String("hi I am here");
+const firstChar = str3.charAt(0);
+console.log("first char is ",firstChar);
+console.log("first char is ",str[0]);
+
+
+
+const indexOfI = str3.indexOf('I');
+console.log("index of I is",indexOfI);
+
+const indexOfi = str3.indexOf('i');
+console.log("index of i is ",indexOfi);
+
+const indexOfe = str3.indexOf('e');
+console.log("index of e is",indexOfe);
+
+
+const indexOflaste = str3.lastIndexOf('e');
+console.log("index Of laste e  is",indexOflaste);
