7c7a2e2755211d5c456fd7ab110253d677971dd3
Mohd Danish <alid41549@gmail.com>
2023-08-21 10:32:28 +0530

    added trunary operators

---
+++ javascript/Day6/turnary.js
@@ -0,0 +1,18 @@
+let  x = 9;
+//if x is the multiple of 3 then assign x to y, otherwise assign 0 to y
+
+let y;
+if(x%3==0){
+    y=x;
+}else{
+    y=0;
+}
+console.log("using if",y);
+
+//if x is the multiple of 3 then assign x to y, otherwise assign 0 to y
+
+let z = x%3==0?x:0;
+console.log("using if ",y);
+
+
+
