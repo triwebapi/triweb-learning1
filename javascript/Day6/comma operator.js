5fd2bb80ca376898a8fc990cd18db383566eb0bc
Mohd Danish <alid41549@gmail.com>
2023-08-21 10:41:54 +0530

    added comma operator

---
+++ javascript/Day6/comma operator.js
@@ -0,0 +1,9 @@
+let xy =(3,4,5,6);
+console.log(xy);// returned last value
+
+
+
+for(let x=0,y =3; x<2;x++,y++){
+    console.log("x=",x);
+    console.log("y=",y); // here we are not taking value oly check condition .(use to handle multiple valyue in one lopp)
+}
