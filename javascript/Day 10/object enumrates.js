e0157ef94f5f5eba09a920959936029636e4689e
Mohd Damish <danish.dmseo@gmail.com>
2023-08-25 16:00:32 -0700

    added object Enumrate

---
+++ javascript/Day 10/object enumrates.js
@@ -0,0 +1,5 @@
+let Obj3 = {name:'Danish', address: "India",mobile: "989123", email:"danish.dmseo@gmail.com"};
+for (const key in Obj3) {
+    console.log(key);
+    console.log(`values on ${key} is ${obj[key]}`);
+}
